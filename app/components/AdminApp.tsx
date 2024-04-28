7576b00c47d0882669b12bbf07375b588d44c75f
HRojotiana <hei.rojotiana@gmail.com>
2024-04-28 23:54:57 +0300

    feat: updated Admin app

--- app/components/AdminApp.tsx
+++ app/components/AdminApp.tsx
@@ -1,19 +1,61 @@
 "use client"
 
-import { Admin, Resource, ListGuesser, EditGuesser } from "react-admin"
+import React from "react"
 import jsonServerProvider from "ra-data-json-server"
+import { Admin, Datagrid, EditButton, Filter, FunctionField, List, ReferenceField, Resource, TextField, TextInput, useRecordContext } from "react-admin"
+import { useNavigate } from "react-router-dom";
+import CommentButton from "./CommentButton";
 
-const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com")
+const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
-const AdminApp = () => (
+const PostList: React.FC = (props) => {
+    const navigate = useNavigate();
+
+    const CommentButton = () => {
+        const record = useRecordContext();
+        return <button onClick={() => navigate(`/comments?filter={"postId":${record.id}}`)}> View Comments </button>
+    }
+
+return (
+    <List {...props}>
+        <Datagrid>
+            <TextField source="id"/>
+            <TextField source="title" />
+            <ReferenceField source="userId" reference="users">
+                <TextField source="name" />
+            </ReferenceField>
+            <FunctionField label="Comments" render={() =><CommentButton/>}/>
+            <EditButton/>
+        </Datagrid>
+    </List>
+);
+};
+
+const CommentFilter: React.FC = (props) => (
+    <Filter {...props}>
+        <TextInput label="Post ID" source="postId" alwaysOn/>
+    </Filter>
+);
+
+const CommentList: React.FC = (props) => (
+    <List {...props} filters={<CommentFilter/>}>
+        <Datagrid>
+            <TextField source ="id" />
+            <TextField source = "name" label="Post title"/>
+            <ReferenceField source="postId" reference="posts">
+                <TextField source="title"/>
+            </ReferenceField>
+            <TextField source="email"/> 
+            <TextField source="body"/>
+            <EditButton/>
+        </Datagrid>
+    </List>
+);
+
+const AdminApp: React.FC = () => (
     <Admin dataProvider = {dataProvider}>
-        <Resource 
-        name="posts"
-        list={ListGuesser}
-        edit={EditGuesser}
-        recordRepresentation="title"/>
-
-        <Resource name="comments" list={ListGuesser} edit={EditGuesser}/>
+        <Resource name = "posts" list={PostList}/>
+        <Resource name="comments" list={CommentList}/>
     </Admin>
 );
 
--- app/page.tsx
+++ app/page.tsx
@@ -5,4 +5,8 @@ const AdminApp = dynamic(() => import("@/app/components/AdminApp"),{ssr:false});
 
 const Home: NextPage = () => <AdminApp/>;
 
+// function Home(){
+//   return <div>Hello World</div>
+// }
+
 export default Home;
